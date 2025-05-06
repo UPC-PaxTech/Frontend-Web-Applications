7c3c62c981b0a46ebb94ae7bb145171ee67931c0
Suin Yum <u202210838@upc.edu.pe>
2025-05-05 23:29:16 -0500

    feat: adddd example test of reservation model and services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {ReservationApiService} from './reservation/services/reservation-api.service';
+import {Reservation} from './reservation/model/reservation.entity';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -7,6 +9,19 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Frontend-Web-Applications';
+
+  constructor(private reservationApi: ReservationApiService) {}
+
+  ngOnInit(): void {
+    this.reservationApi.getReservations().subscribe({
+      next: (reservations: Reservation[]) => {
+        console.log('📦 Reservaciones cargadas:', reservations);
+      },
+      error: (err) => {
+        console.error('❌ Error al cargar reservaciones:', err);
+      }
+    })
+  }
 }
