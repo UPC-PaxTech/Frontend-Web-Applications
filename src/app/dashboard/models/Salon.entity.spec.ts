71430cc06570376c5a76dcf8dc152d25bff6d16f
Krillsom <kevinjchic@gmail.com>
2025-05-08 13:33:21 -0500

    feature: add Salon entity
    # Conflicts:
    #	angular.json
    #	src/app/app.component.html
    #	src/app/app.component.ts

---
+++ src/app/dashboard/models/Salon.entity.spec.ts
@@ -0,0 +1,7 @@
+import { Salons } from './Salon.entity';
+
+describe('Salons', () => {
+  it('should create an instance', () => {
+    expect(new Salons()).toBeTruthy();
+  });
+});
---
+++ src/app/dashboard/models/Salon.entity.ts
@@ -0,0 +1,15 @@
+export class Salon {
+  salonId: string;
+  name: string;
+  location: string;
+  phone: number;
+  imageURL: string;
+
+  constructor() {
+    this.salonId = '';
+    this.name = '';
+    this.location = '';
+    this.phone = 0;
+    this.imageURL = '';
+  }
+}
