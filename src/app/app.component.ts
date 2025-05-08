5a329e15f46a55a0514cc9ff19a404db3cb63421
jsyumg <117481269+jsyumg@users.noreply.github.com>
2025-05-08 12:43:36 -0500

    feat: added reviews in fake api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,13 @@ import { RouterOutlet } from '@angular/router';
 import {
   UpcomingAppointmentsComponent
 } from './dashboard/components/upcoming-appointments/upcoming-appointments.component';
+import {
+  ProfessionalDashboardComponent
+} from './dashboard/pages/professional-dashboard/professional-dashboard.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, UpcomingAppointmentsComponent],
+  imports: [RouterOutlet, UpcomingAppointmentsComponent, ProfessionalDashboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
