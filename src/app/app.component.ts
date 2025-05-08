1b02412d6b9a54a0021a4611327b27ab6bee3ee0
Krillsom <kevinjchic@gmail.com>
2025-05-08 15:38:01 -0500

    fix: fix salon cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,12 @@ import { RouterOutlet } from '@angular/router';
 import {
   UpcomingAppointmentsComponent
 } from './dashboard/components/upcoming-appointments/upcoming-appointments.component';
-import {SalonItemComponent} from './dashboard/components/salon-item/Salon-item.component';
+
 import {SalonListComponent} from './dashboard/components/salon-list/salon-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, UpcomingAppointmentsComponent, SalonItemComponent, SalonListComponent],
+  imports: [RouterOutlet, UpcomingAppointmentsComponent, SalonListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
