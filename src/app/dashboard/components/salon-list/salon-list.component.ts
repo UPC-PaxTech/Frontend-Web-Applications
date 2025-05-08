5119768ed1ba6678fe2a2357d211379c0662d46f
Krillsom <kevinjchic@gmail.com>
2025-05-08 15:38:01 -0500

    fix: fix salon cards

--- src/app/dashboard/components/salon-list/salon-list.component.ts
+++ src/app/dashboard/components/salon-list/salon-list.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { SalonApiService } from '../../services/salon-api.service';
 import {Salon} from '../../models/Salon.entity';
+import {SalonItemComponent} from '../salon-item/Salon-item.component';
 
 @Component({
   selector: 'app-salon-list',
+  imports: [
+    SalonItemComponent
+  ],
   templateUrl: './salon-list.component.html'
 })
 export class SalonListComponent implements OnInit {
@@ -14,6 +18,7 @@ export class SalonListComponent implements OnInit {
   ngOnInit(): void {
     this.salonApiService.getSalons().subscribe(salons => {
       this.salons = salons;
+      console.log(salons);
     });
   }
 }
