1c9c99fa292310ade878428cf8f13d36ce6d2205
Gael Rs <gaelrszk@gmail.com>
2025-05-08 11:27:30 -0500

    feat: add schedule bounded context

---
+++ src/app/schedule/components/calendar/calendar.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calendar',
+  templateUrl: './calendar.component.html',
+  styleUrls: ['./calendar.component.css']
+})
+export class CalendarComponent {
+  days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+  hours = [
+    '14:00 - 15:00',
+    '15:00 - 16:00',
+    '16:00 - 17:00',
+    '17:00 - 18:00',
+    '18:00 - 19:00',
+    '19:00 - 20:00'
+  ];
+}
