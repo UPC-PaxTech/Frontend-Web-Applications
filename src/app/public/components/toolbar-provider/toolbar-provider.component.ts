0adc0908cce9c4fe45d756ebb7634e2ca22db4a8
Gael Rs <gaelrszk@gmail.com>
2025-05-09 00:33:47 -0500

    feat: update toolbar function

--- src/app/public/components/toolbar-provider/toolbar-provider.component.ts
+++ src/app/public/components/toolbar-provider/toolbar-provider.component.ts
@@ -1,19 +1,15 @@
   import { Component } from '@angular/core';
   import { CommonModule } from '@angular/common';
   import { RouterModule } from '@angular/router';
+  import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 
   @Component({
     selector: 'app-toolbar-provider',
-    imports: [RouterModule, CommonModule],
+    imports: [RouterModule, CommonModule, RouterLink, RouterLinkActive, RouterOutlet],
     templateUrl: './toolbar-provider.component.html',
     styleUrl: './toolbar-provider.component.css'
   })
   export class ToolbarProviderComponent {
 
-    options = [
-      {link: '/home-provider', label: 'home-provider'},
-      {link: '/logout', label: 'logout'},
-      {link: '/profile-provider', label: 'profile-provider'},
-    ]
 
   }
