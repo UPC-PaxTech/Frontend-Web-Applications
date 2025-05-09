8ced9860e658b23fdeab343cace3ab2b17645bb2
Gael Rs <gaelrszk@gmail.com>
2025-05-08 20:19:16 -0500

    feat: final version of toolbar design

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+//import {HomeProviderComponent} from './public/pages/home/home.component'; // HomeProvides (Dashboard)
+//const ProfileProviderComponent = () => import('./public/pages/about/about.component').then(m => m.AboutComponent); // Logout
+//const LogoutComponent = () => import('./public/pages/about/about.component').then(m => m.AboutComponent); // Profile Provider
+
+
+export const routes: Routes = [
+  // { path: 'profile-provider',  component:      ProfileProviderComponent },
+  // { path: 'logout',            loadComponent:  LogoutComponent },
+  // { path: 'home-provider',     loadComponent:  HomeProviderComponent },
+];
--- src/app/public/components/toolbar-provider/toolbar-provider.component.ts
+++ src/app/public/components/toolbar-provider/toolbar-provider.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+  import { Component } from '@angular/core';
+  import { CommonModule } from '@angular/common';
+  import { RouterModule } from '@angular/router';
 
-@Component({
-  selector: 'app-toolbar-provider',
-  imports: [],
-  templateUrl: './toolbar-provider.component.html',
-  styleUrl: './toolbar-provider.component.css'
-})
-export class ToolbarProviderComponent {
+  @Component({
+    selector: 'app-toolbar-provider',
+    imports: [RouterModule, CommonModule],
+    templateUrl: './toolbar-provider.component.html',
+    styleUrl: './toolbar-provider.component.css'
+  })
+  export class ToolbarProviderComponent {
 
-}
+    options = [
+      {link: '/home-provider', label: 'home-provider'},
+      {link: '/logout', label: 'logout'},
+      {link: '/profile-provider', label: 'profile-provider'},
+    ]
+
+  }
