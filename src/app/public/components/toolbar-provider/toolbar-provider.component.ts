49538d253759b2b1b4c7e0b4cba3573d795112d9
Gael Rs <gaelrszk@gmail.com>
2025-05-08 19:34:01 -0500

    feat: design toolbar layout and styling

---
+++ src/app/public/components/toolbar-provider/toolbar-provider.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-toolbar-provider',
+  imports: [],
+  templateUrl: './toolbar-provider.component.html',
+  styleUrl: './toolbar-provider.component.css'
+})
+export class ToolbarProviderComponent {
+
+}
