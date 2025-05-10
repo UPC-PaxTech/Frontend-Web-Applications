1d09d06ef61856889b54bd12ce20e4fc9f7a228d
Gael Rs <gaelrszk@gmail.com>
2025-05-09 22:41:11 -0500

    feat: add login page

--- src/app/iam/components/login-form/login-form.component.ts
+++ src/app/iam/components/login-form/login-form.component.ts
@@ -1,8 +1,22 @@
 import { Component } from '@angular/core';
+import {MatCard, MatCardActions, MatCardContent, MatCardHeader} from '@angular/material/card';
+import {MatButton} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInput} from '@angular/material/input';
+import {MatCardTitle} from '@angular/material/card';
 
 @Component({
   selector: 'app-login-form',
-  imports: [],
+  imports: [
+    MatCardActions,
+    MatCardHeader,
+    MatCard,
+    MatCardContent,
+    MatButton,
+    MatFormFieldModule,
+    MatInput,
+    MatCardTitle
+  ],
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.css'
 })
--- src/app/iam/pages/login-page/login-page.component.ts
+++ src/app/iam/pages/login-page/login-page.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import {LoginFormComponent} from '../../components/login-form/login-form.component';
+import {MatToolbar} from '@angular/material/toolbar';
+import {MatButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  imports: [
+    LoginFormComponent,
+    MatToolbar,
+    MatButton
+  ],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
