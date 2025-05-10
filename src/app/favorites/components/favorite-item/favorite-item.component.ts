481e7f91b8f36f42188173a868fdaaf890238306
Marcelo <166082569+VarBus@users.noreply.github.com>
2025-05-10 14:59:39 -0500

    feat: add item and list components with response and assembler

---
+++ src/app/favorites/components/favorite-item/favorite-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-favorite-item',
+  imports: [],
+  templateUrl: './favorite-item.component.html',
+  styleUrl: './favorite-item.component.css'
+})
+export class FavoriteItemComponent {
+
+}
---
+++ src/app/favorites/components/favorite-list/favorite-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-favorite-list',
+  imports: [],
+  templateUrl: './favorite-list.component.html',
+  styleUrl: './favorite-list.component.css'
+})
+export class FavoriteListComponent {
+
+}
