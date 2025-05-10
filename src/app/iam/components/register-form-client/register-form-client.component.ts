483cd2340e532ae990bf1521faacd3cbd90643c3
Gael Rs <gaelrszk@gmail.com>
2025-05-09 19:00:54 -0500

    feat: create all compoenents of the iam bounded context

---
+++ src/app/iam/components/register-form-client/register-form-client.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-form-client',
+  imports: [],
+  templateUrl: './register-form-client.component.html',
+  styleUrl: './register-form-client.component.css'
+})
+export class RegisterFormClientComponent {
+
+}
---
+++ src/app/iam/components/register-form-provider/register-form-provider.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-form-provider',
+  imports: [],
+  templateUrl: './register-form-provider.component.html',
+  styleUrl: './register-form-provider.component.css'
+})
+export class RegisterFormProviderComponent {
+
+}
