2b33e12b151e62ad7ec4541de734d5f5d676e25d
Marcelo <166082569+VarBus@users.noreply.github.com>
2025-05-10 16:02:33 -0500

    feat: add favorites api service

--- src/app/favorites/services/favorite.assembler.ts
+++ src/app/favorites/services/favorite.assembler.ts
@@ -1,2 +1,16 @@
-export class Favorite {
+import {FavoriteResource} from './favorite.resource';
+import {Favorite} from '../model/favorite.entity';
+
+export class FavoriteAssembler {
+  static  toEntityFromResource(resource: FavoriteResource): Favorite {
+    return {
+      id: resource.workerId,
+      nombre: resource.nombre,
+      especializacion: resource.especializacion,
+      foto: resource.fotoUrl
+    }
+  }
+  static toEntitiesFromResponse(resources: FavoriteResource[]): Favorite[] {
+    return resources.map(resource => this.toEntityFromResource(resource));
+  }
 }
