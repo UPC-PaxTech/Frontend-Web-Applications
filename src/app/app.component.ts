70e99bbc32c4c36cfcdae3593559fd7dd61c49dd
Marcelo <166082569+VarBus@users.noreply.github.com>
2025-05-09 23:45:02 -0500

    feat: add changes css routes and toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,12 +24,14 @@ import {TranslateService} from '@ngx-translate/core';
 import {AppointmentsListComponent} from './appointments/components/appointments-list/appointments-list.component';
 import {ToolbarProviderComponent} from './public/components/toolbar-provider/toolbar-provider.component';
 import {SidebarComponent} from './public/components/sidebar/sidebar.component';
+import {SidebarClientComponent} from './public/components/sidebar-client/sidebar-client.component';
+import {ToolbarClientComponent} from './public/components/toolbar-client/toolbar-client.component';
 
 
 @Component({
   selector: 'app-root',
 
-  imports: [RouterOutlet, AppointmentsListComponent, ToolbarProviderComponent, SidebarComponent
+  imports: [RouterOutlet, AppointmentsListComponent, ToolbarProviderComponent, SidebarComponent, SidebarClientComponent, ToolbarClientComponent
 
   ],
 
