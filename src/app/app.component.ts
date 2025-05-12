47738ecfd7a7a68944eaa66d6e45f85bd323f429
Krillsom <kevinjchic@gmail.com>
2025-05-12 00:56:37 -0500

    feature: add dashboard client

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 import { RouterOutlet } from '@angular/router';
-
-import { UpcomingAppointmentsComponent } from './dashboard/components/upcoming-appointments/upcoming-appointments.component';
-import { ProfessionalDashboardComponent } from './dashboard/pages/professional-dashboard/professional-dashboard.component';
-
-import {CalendarComponent} from './schedule/components/calendar/calendar.component'
 import { ToolbarProviderComponent } from './public/components/toolbar-provider/toolbar-provider.component'
 
-import {ProfilePageComponent} from './profile/pages/profile-page/profile-page.component';
 import {SidebarComponent} from './public/components/sidebar/sidebar.component';
 import {TranslateService} from '@ngx-translate/core';
+import {ToolbarClientComponent} from './public/components/toolbar-client/toolbar-client.component';
+import {SidebarClientComponent} from './public/components/sidebar-client/sidebar-client.component';
 
 @Component({
   selector: 'app-root',
