2f9ce652ea56247ce8a4240f315be5ab8958fe89
Marcelo <166082569+VarBus@users.noreply.github.com>
2025-05-13 10:51:40 -0500

    feat: add favorite section

--- src/app/favorites/pages/client-favorite/client-favorite.component.ts
+++ src/app/favorites/pages/client-favorite/client-favorite.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-import {FavoriteListComponent} from '../../components/favorite-list/favorite-list.component';
+import {
+  UpcomingAppointmentsComponent
+} from '../../../appointments/components/upcoming-appointments/upcoming-appointments.component';
+import {SidebarClientComponent} from '../../../public/components/sidebar-client/sidebar-client.component';
+import {SalonListComponent} from '../../../dashboard/components/salon-list/salon-list.component';
 
 @Component({
   selector: 'app-client-favorite',
   imports: [
-    FavoriteListComponent
+    UpcomingAppointmentsComponent,
+    SidebarClientComponent,
+    SalonListComponent
   ],
   templateUrl: './client-favorite.component.html',
   styleUrl: './client-favorite.component.css'
