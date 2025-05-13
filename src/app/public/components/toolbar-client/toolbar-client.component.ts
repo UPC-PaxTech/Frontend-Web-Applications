baa0008de227a0776aa174196dfc5a25afe91bca
Marcelo <166082569+VarBus@users.noreply.github.com>
2025-05-13 07:50:14 -0500

    fix: solve troll problems

--- src/app/public/components/toolbar-client/toolbar-client.component.ts
+++ src/app/public/components/toolbar-client/toolbar-client.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import {LanguageSwitcherComponent} from '../language-switcher/language-switcher.component';
-import {RouterLink} from '@angular/router';
+import {RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-toolbar-client',
-  imports: [
-    LanguageSwitcherComponent,
-    RouterLink
-  ],
+    imports: [
+        LanguageSwitcherComponent,
+        RouterLink,
+        RouterLinkActive
+    ],
   templateUrl: './toolbar-client.component.html',
   styleUrl: './toolbar-client.component.css'
 })
