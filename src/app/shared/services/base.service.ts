6a273862fb4bf4de66ca72b51f308ff75c8421dd
Krillsom <kevinjchic@gmail.com>
2025-05-14 03:58:57 -0500

    docs: finish feature

--- src/app/shared/services/base.service.ts
+++ src/app/shared/services/base.service.ts
@@ -36,11 +36,6 @@ export abstract class BaseService<R> {
     );
   }
 
-  public create(id: any, resource:R): Observable<R> {
-    return this.http.post<R>(this.resourcePath(), JSON.stringify(resource), this.httpOptions)
-      .pipe(retry(2), catchError(this.handleError));
-  }
-
   public delete(id: any): Observable<any> {
     return this.http.delete(`${this.resourcePath()}/${id}`, this.httpOptions)
       .pipe(retry(2), catchError(this.handleError));
