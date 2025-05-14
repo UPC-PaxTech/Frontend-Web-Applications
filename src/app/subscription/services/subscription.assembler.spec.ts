419296b6cce00fe5252cb22c141c7f107d794207
aaaaangie <angieyalan17@gmail.com>
2025-05-13 20:11:22 -0500

    docs: add subscription tab

--- src/app/subscription/services/subscription.assembler.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Subscription } from './subscription.assembler';
-
-describe('Subscription', () => {
-  it('should create an instance', () => {
-    expect(new Subscription()).toBeTruthy();
-  });
-});
