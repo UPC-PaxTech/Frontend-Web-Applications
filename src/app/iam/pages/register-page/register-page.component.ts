806108482599a67848b49aeab3efe2630bd35038
Gael Rs <gaelrszk@gmail.com>
2025-05-23 15:00:08 -0500

    feat: Add a new UIX form to the login and registration pages

--- src/app/iam/pages/register-page/register-page.component.ts
+++ src/app/iam/pages/register-page/register-page.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import {RegisterFormClientComponent} from '../../components/register-form-client/register-form-client.component';
 import {RegisterFormProviderComponent} from '../../components/register-form-provider/register-form-provider.component';
 import {FormsModule} from '@angular/forms';
-import {NgIf} from '@angular/common';
+import {NgForOf, NgIf} from '@angular/common';
 import { MatButtonToggleGroup} from '@angular/material/button-toggle';
 import {MatButtonToggle} from '@angular/material/button-toggle';
 import {LoginFormComponent} from '../../components/login-form/login-form.component';
@@ -22,16 +22,39 @@ import {RouterLink} from '@angular/router';
     LoginFormComponent,
     MatButton,
     MatToolbar,
-    RouterLink
+    RouterLink,
+    NgForOf
   ],
   templateUrl: './register-page.component.html',
   styleUrl: './register-page.component.css'
 })
 export class RegisterPageComponent {
   isProvider: boolean = false;  // Determina si se está registrando un cliente o proveedor
+  slideImages: string[] = [
+    'https://www.gammabross.com/Gallery/salonimg-frkqkj-181.webp',
+    'https://thehappening.com/wp-content/uploads/2024/02/captura-de-pantalla-2023-05-17-a-la-s-52813-pm-1.jpg',
+    'https://cdn1.treatwell.net/images/view/v2.i7379851.w720.h480.x5F15B4CB/'
+  ];
 
+  activeIndex: number = 0;
+  intervalId: any;
+
+  ngOnInit(): void {
+    this.intervalId = setInterval(() => {
+      this.activeIndex = (this.activeIndex + 1) % this.slideImages.length;
+    }, 5000); // cambia cada 5 segundos
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.intervalId);
+  }
+
+  setSlide(index: number): void {
+    this.activeIndex = index;
+  }
   // Cambia entre los formularios de cliente y proveedor
   toggleForm(isProvider: boolean): void {
     this.isProvider = isProvider;
   }
 }
+
