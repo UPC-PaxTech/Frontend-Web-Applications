6d5fecef17e2a3f77f20a60774ef150f4070b5aa
Gael Rs <gaelrszk@gmail.com>
2025-05-24 18:56:52 -0500

    feat: update ui about provider layout

--- src/app/public/components/toolbar-provider/toolbar-provider.component.ts
+++ src/app/public/components/toolbar-provider/toolbar-provider.component.ts
@@ -1,12 +1,15 @@
   import { Component } from '@angular/core';
   import { CommonModule } from '@angular/common';
   import { RouterModule } from '@angular/router';
-  import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+  import {RouterLink} from '@angular/router';
   import {LanguageSwitcherComponent} from '../language-switcher/language-switcher.component';
+  import {FormsModule} from '@angular/forms';
+  import {MatAutocomplete, MatAutocompleteTrigger, MatOption} from '@angular/material/autocomplete';
+  import {MatInput} from '@angular/material/input';
 
   @Component({
     selector: 'app-toolbar-provider',
-    imports: [RouterModule, CommonModule, RouterLink, RouterLinkActive, RouterOutlet, LanguageSwitcherComponent],
+    imports: [RouterModule, CommonModule, RouterLink, LanguageSwitcherComponent, FormsModule, MatAutocomplete, MatAutocompleteTrigger, MatInput, MatOption],
     templateUrl: './toolbar-provider.component.html',
     styleUrl: './toolbar-provider.component.css'
   })
