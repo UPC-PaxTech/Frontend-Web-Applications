4d9e72f7186c8b5f7dd65475d73da0218a9a530e
Gael Rs <gaelrszk@gmail.com>
2025-05-23 19:27:47 -0500

    feat: add login verification

--- src/app/iam/components/login-form/login-form.component.ts
+++ src/app/iam/components/login-form/login-form.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import {MatButton} from '@angular/material/button';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormBuilder, FormGroup,  Validators } from '@angular/forms';
 import {MatInput} from '@angular/material/input';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {RouterLink} from '@angular/router';
+import {NgIf} from '@angular/common';
+import {AccountApiService} from '../../services/accountApi.service';
+import { Router, RouterLink } from '@angular/router';
+import {AccountEntity} from '../../model/account.entity';
+
 
 @Component({
   selector: 'app-login-form',
@@ -14,10 +19,44 @@ import {RouterLink} from '@angular/router';
     FormsModule,
     ReactiveFormsModule,
     RouterLink,
+    NgIf,
   ],
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.css'
 })
 export class LoginFormComponent {
+  loginForm: FormGroup;
+  loginError = false;
 
+  constructor(
+    private fb: FormBuilder,
+    private accountApiService: AccountApiService,
+    private router: Router
+  ) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    const { email, password } = this.loginForm.value;
+
+    this.accountApiService.getAccounts().subscribe((accounts: AccountEntity[]) => {
+      const account = accounts.find(
+        acc => acc.email === email && acc.passwordHash === password && acc.isActive
+      );
+
+      if (account) {
+        this.loginError = false;
+        if (account.type === 'provider') {
+          this.router.navigate(['/provider/homeProvider']);
+        } else if (account.type === 'client') {
+          this.router.navigate(['/client/homeClient']);
+        }
+      } else {
+        this.loginError = true;
+      }
+    });
+  }
 }
